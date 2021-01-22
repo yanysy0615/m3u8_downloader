15b4892d9646120359bb60c8feb14361529606a7
yanshuoyan <yanysy0615@qq.com>
2021-01-23 00:58:21 +0800

    1

--- ui_mode/lib/processors/simple_lock.py
+++
@@ -1,9 +0,0 @@
-class SimpleLock:
-    def __init__(self, val=False):
-        self.val = val
-
-    def is_locked(self):
-        return self.val
-    
-    def set(self, val):
-        self.val = val
